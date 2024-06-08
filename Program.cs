3ea3bf37f517a2a27bee6f5448340f46af006b7f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-07 21:22:09 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- ENTRADA VÁLIDA ---");
+
+int resposta;
+do 
+{
+
+    Console.Write("\nDigite um número: ");
+    resposta = Convert.ToInt32(Console.ReadLine())!;
+
+}
+while(resposta < 0 || resposta >= 10);
+
+if (resposta == 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("\nOperação cancelada");
+    Console.ResetColor();
+}
+
+else
+{
+    Console.WriteLine($"\nNúmero selecionado = {resposta}");
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("\nVolte sempre!");
+    Console.ResetColor();
+}
